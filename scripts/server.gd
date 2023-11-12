c475a782279428fb647f81e8fad7a51e01d20aba
Maroun Baydoun <maroun.baydoun@gmail.com>
2023-11-12 14:06:27 +0100

    Add price label to server

--- scripts/server.gd
+++ scripts/server.gd
@@ -5,9 +5,10 @@ class_name Server
 const INITIAL_COMPUTING_POWER: int = 100
 
 var computing_power: int = INITIAL_COMPUTING_POWER
-
+var price: int = 1000
 
 @onready var height: float = mesh.size.y
+@onready var price_label: Label3D = $PriceLabel
 	
 
 	
@@ -20,6 +21,18 @@ func appear(origin: Vector3):
 	
 	tween.tween_property(self, "transform:origin:y", height / 2, 0.5)
 	
+	await get_tree().create_timer(0.3)
+	
+	animate_price()
 
 
-
+func animate_price():
+	price_label.text = str(-1 * price)
+	price_label.transform.origin.y = 0
+	
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	
+	tween.tween_property(price_label, "transform:origin:y", height * 1.5, 0.7)
+	await get_tree().create_timer(0.5)
+	tween.tween_property(price_label, "modulate:a", 0, 0.2)
