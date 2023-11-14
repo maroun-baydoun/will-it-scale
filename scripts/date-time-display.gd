b175e534a4a1bb6908ba36d24794976c9c717783
Maroun Baydoun <maroun.baydoun@gmail.com>
2023-11-14 22:25:12 +0100

    Display hours and minutes

--- scripts/date-time-display.gd
+++ scripts/date-time-display.gd
@@ -1,11 +1,37 @@
 extends Control
 class_name DateTimeDisplay
 
-@onready var label: Label = $MarginContainer/Label
+@onready var date_label: Label = %DateLabel
+@onready var time_label: Label = %TimeLabel
+@onready var minutes_timer: Timer = $MinutesTimer
 
-const DATE_TIME_FORMAT = "%s/%s/%s"
+const DATE_FORMAT: String = "%s/%s/%s"
+const TIME_FORMAT: String = "%s:%s"
+const ZERO_PADDED_FORMAT: String = "0%s"
+
+var current_minutes: int = 0
 
 var current_date_time: Dictionary:
 	set(date_time):
+		
+		minutes_timer.stop()
+		current_minutes = 0
 		current_date_time = date_time
-		label.text = DATE_TIME_FORMAT % [date_time.day, date_time.month, date_time.year]
+		
+		_update_date_label_text()
+		_update_time_label_text()
+
+		minutes_timer.start()
+
+func _update_date_label_text() -> void:
+	date_label.text = DATE_FORMAT % [_left_pad_with_zero(current_date_time.day), _left_pad_with_zero(current_date_time.month), _left_pad_with_zero(current_date_time.year)]
+
+func _update_time_label_text() -> void:
+	time_label.text = TIME_FORMAT % [_left_pad_with_zero(current_date_time.hour), _left_pad_with_zero(current_minutes)]
+
+func _left_pad_with_zero(number: int) -> String:
+	return ZERO_PADDED_FORMAT % number if number <10 else str(number)
+
+func _on_minutes_timer_timeout() -> void:
+	current_minutes +=1
+	_update_time_label_text()
--- scripts/date-time-manager.gd
+++ scripts/date-time-manager.gd
@@ -8,6 +8,8 @@ signal ticked(date: Dictionary)
 var time_elapsed_in_second: int = 0
 var time_zone: Dictionary = Time.get_time_zone_from_system()
 var current_unix_time: float = Time.get_unix_time_from_system() + (time_zone.bias * 60)
+var start_unix_time: float = Time.get_unix_time_from_system() + (time_zone.bias * 60)
+
 
 var current_date_time: Dictionary:
 	set(date_time):
@@ -15,6 +17,10 @@ var current_date_time: Dictionary:
 		ticked.emit(current_date_time)
 
 func start() -> void:
+	var start_date_time = Time.get_datetime_dict_from_unix_time(start_unix_time)
+	var seconds_since_start_of_day = start_date_time.hour * 3600 + start_date_time.minute * 60 + start_date_time.second
+	start_unix_time -= seconds_since_start_of_day
+	current_unix_time = start_unix_time
 	timer.start()
 	_set_current_date_time_from_unix_time()
 	
