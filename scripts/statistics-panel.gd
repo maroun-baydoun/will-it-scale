0e98a095c3bb3206597c090616126923552347f7
Maroun Baydoun <maroun.baydoun@gmail.com>
2023-11-26 11:35:46 +0100

    Add tooltip system

--- scripts/statistics-panel.gd
+++ scripts/statistics-panel.gd
@@ -7,6 +7,8 @@ class_name StatisticsPanel
 @onready var average_response_time_label: Label = %AverageResponseTimeLabel
 @onready var current_power_consumption_label: Label = %CurrentPowerConsumptionLabel
 
+@onready var panels = get_tree().get_nodes_in_group("statistics-panel")
+@onready var tooltip: PanelContainer = %Tooltip
 
 var current_funds: float:
 	set(value):
@@ -28,3 +30,18 @@ var current_power_consumption:int:
 	set(value):
 		current_power_consumption_label.text = "%skWh" % value
 		
+func _ready():
+	tooltip.visible = false
+		
+	for panel in panels:
+		panel.mouse_entered.connect(func (): _on_panel_container_mouse_entered(panel))
+		panel.mouse_exited.connect(_on_panel_container_mouse_exited)
+
+func _on_panel_container_mouse_entered(panel: PanelContainer) -> void:
+	tooltip.position.x = panel.global_position.x + panel.size.x + 16
+	tooltip.position.y = panel.global_position.y + panel.size.y / 2 - tooltip.size.y / 2
+	# tooltip.visible = true
+
+
+func _on_panel_container_mouse_exited() -> void:
+	tooltip.visible = false
