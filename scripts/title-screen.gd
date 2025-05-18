9c3f9c12b32da1010a4ff25f8335ba692030c4d6
Maroun Baydoun <maroun.baydoun@gmail.com>
2025-05-19 00:01:49 +0200

    Skip title

--- scripts/title-screen.gd
+++ scripts/title-screen.gd
@@ -1,7 +1,14 @@
 extends Node
 
+func _ready() -> void:
+	var args = OS.get_cmdline_user_args()
+
+	if args.has('--skip-title'):
+		_change_to_main_scene()
+
 func _on_start_button_pressed() -> void:
+	_change_to_main_scene()
+
+func _change_to_main_scene() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
-
-
 	
